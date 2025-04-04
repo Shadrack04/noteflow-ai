b07998bd69af98020e8f30b705f49a0e3b340d24
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-04-04 01:21:07 +0100

    Create the testimonial section

---
+++ src/components/testimonial/TestimonialItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import headshot1 from "/images/headshot-01.png";
+import star from "/images/star.svg";
+
+function TestimonialItem({ testimonial }) {
+  const { rating, message, name, position, avatar } = testimonial;
+  return (
+    <div className=" max-w-[21rem] bg-primary-1300 px-6 py-8 rounded-2xl row-span-2">
+      <div className=" mb-6">
+        <img src={star} alt="" />
+      </div>
+      <p className=" text-secondary-text text-md leading-8 mb-14">{message}</p>
+      <div className=" flex items-center gap-4">
+        <img src={avatar} alt="" className=" size-12" />
+        <div className=" flex flex-col gap-1">
+          <p className=" text-special-text font-bold">{name}</p>
+          <p className="text-secondary-text text-sm">{position}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TestimonialItem;
