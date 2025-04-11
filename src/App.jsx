a0a74064418eec3f0fbda45ab24575d6ed5e629c
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-04-11 02:47:23 +0100

    Create the modal section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Modal from "./components/Modal";
+import { useState } from "react";
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
   return (
     <div className="">
-      <Header />
-      <Main />
+      <Header setOpenModal={setOpenModal} />
+      {openModal && <Modal setOpenModal={setOpenModal} />}
+      <Main setOpenModal={setOpenModal} />
       <Footer />
     </div>
   );
