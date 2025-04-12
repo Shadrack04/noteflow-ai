45c9765bee6b2391fc54f4b2eb154c92c8571971
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-04-12 23:50:55 +0100

    random fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PiSquaresFourBold } from "react-icons/pi";
 import Logo from "./Logo";
 import PageNav from "./PageNav";
 
@@ -7,6 +8,7 @@ function Header({ setOpenModal }) {
     <header className=" custom-padding bg-primary-1500 flex items-center justify-between py-2">
       <Logo />
       <PageNav setOpenModal={setOpenModal} />
+      <PiSquaresFourBold className=" md:hidden text-secondary-text size-10 cursor-pointer" />
     </header>
   );
 }
