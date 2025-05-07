61c733d4e9185538114bb156811eb35b86352101
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-05-07 12:49:43 +0100

    implement the testimonial section with the help of a third party library react-masonry-css

--- src/components/SetDarkMode.jsx
+++ src/components/SetDarkMode.jsx
@@ -5,7 +5,10 @@ import { MdDarkMode } from "react-icons/md";
 
 function SetDarkMode({ darkMode, setDarkMode }) {
   return (
-    <div onClick={() => setDarkMode((dark) => !dark)}>
+    <div
+      onClick={() => setDarkMode((dark) => !dark)}
+      className=" cursor-pointer"
+    >
       {darkMode ? (
         <MdOutlineLightMode className=" fixed bottom-6 right-6 md:right-10 lg:right-10 xl:right-22 medium-laptop:right-[8rem] 2xl:right-[42rem] size-10 text-white" />
       ) : (
